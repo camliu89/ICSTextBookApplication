d4b378ed1d506c6298165fc9009ddbf84919a6b5
Alexander Cam Liu <alexcamliu@gmail.com>
2013-05-06 23:49:36 -1000

    Playing with views

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,20 @@
 package controllers;
 
+import java.util.List;
+import models.Student;
 import play.*;
 import play.mvc.*;
-
 import views.html.*;
 
 public class Application extends Controller {
+
   
-    public static Result index() {
-        return ok(index.render("Your new application is ready."));
-    }
   
+  public static Result index() {
+    Student student = new Student("Student-1", "John Smith", "smith@hawaii.edu");
+    student.save();
+    List<models.Offer> offers = student.getOffers();
+    return ok(index.render(student.getStudName(), offers));
+  }
+
 }
